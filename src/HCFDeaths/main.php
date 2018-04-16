8dd023d327e0dc54833697e8f3ecf46de2485805
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-16 12:54:05 +0100

    Fixed syntax error.

--- src/HCFDeaths/main.php
+++ src/HCFDeaths/main.php
@@ -19,7 +19,6 @@ use pocketmine\level\Level;
 use pocketmine\Server;
 use pocketmine\entity\Effect;
 use pocketmine\item\enchantment\Enchantment;
-
 use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\event\player\PlayerJoinEvent;
@@ -39,7 +38,6 @@ use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 
 class main extends PluginBase implements Listener{
-
 public function sendStatus($msg){
 $url = "https://discordapp.com/api/webhooks/419842008283086850/7F6BfX9_lp2KU9jmz1UXoRSZnzKx0AigRV5mtsg_n9PiWX4ZNdI8Y7bDLUd-7bd47UmZ";
 $data = array("content" => $msg, "username" => "Matts plugins.");
@@ -49,13 +47,11 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 return curl_exec($curl);
 }
-
 public function onEnable(){
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 /*$this->saveDefaultConfig();*/
 $this->sendStatus($this->getServer()->getMotd() . " just enabled HCFDeaths");
 }
-
 public function deathBan($p, $str){
 $path = "deaths/";
 $file = strtolower($p);
@@ -64,7 +60,6 @@ if(!file_exists($this->getDataFolder() . $path . $file . ".yml")){
 $config = new Config($this->getDataFolder() . $path . $file . ".yml", Config::YAML, ["time" => $str]);
 }
 }
-
 public function undeathBan($p){
 $path = "deaths/";
 $file = strtolower($p);
@@ -72,7 +67,6 @@ if(file_exists($this->getDataFolder() . $path . $file . ".yml")){
 unlink($this->getDataFolder() . $path . $file . ".yml");
 }
 }
-
 public function banExists($p): bool {
 $path = "deaths/";
 $file = strtolower($p);
@@ -82,7 +76,6 @@ return true;
 return false;
 }
 }
-
 public function getBanTime($p): int {
 $path = "deaths/";
 $file = strtolower($p);
@@ -92,12 +85,13 @@ $current = $config2->get("time");
 return $current;
 }
 }
-
 public function addLife($p, $str){
 $path = "lives/";
 $file = strtolower($p);
 if(!file_exists($this->getDataFolder() . $path . $file . ".yml")){
 @mkdir($this->getDataFolder() . $path, 0777, true);
+}
+}
 public function removeLife($p){
 $path = "lives/";
 $file = strtolower($p);
@@ -105,7 +99,6 @@ if(file_exists($this->getDataFolder() . $path . $file . ".yml")){
 unlink($this->getDataFolder() . $path . $file . ".yml");
 }
 }
-
 public function handleLife($p){
 $path = "lives/";
 $file = strtolower($p);
@@ -117,7 +110,6 @@ $this->removeLife($p);
 }
 }
 }
-
 public function onDeath(PlayerDeathEvent $event){
 $ent = $event->getEntity();
 $cause = $ent->getLastDamageCause();
@@ -127,12 +119,10 @@ $weapon = $killer->getInventory()->getItemInHand()->getName();
 $event->setDeathMessage("ยงe" . $ent->getName() . " was slain by " . $killer->getName() . " using " . $weapon);
 }
 }
-
 public function onRespawn(PlayerRespawnEvent $event){
 $old = microtime();
 $this->deathBan($ent, $old);
 }
-
 public function onJoin(PlayerJoinEvent $event){
 $p = $event->getPlayer();
 $new = microtime();
@@ -144,8 +134,7 @@ $p->kick(TextFormat::RED . "Death banned.\nPlease wait untill your ban time is o
 }
 }
 }
-
-public function onCommand(CommandSender $sender, Command $command, $label, array $args): bool {
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 switch(strtolower($command->getName())){
 case "addlive":
 if(isset($args[0])){
@@ -154,5 +143,4 @@ $p = $this->getServer()->getPlayer($args[0]);
 break;
 }
 }
-
 }
